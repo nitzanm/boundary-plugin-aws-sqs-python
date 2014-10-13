947e68cbc272932ffe599e1487fa96327e78143b
Nitzan Miron <bug.assembla@bugbug.me>
2014-10-13 08:26:21 -0700

    Removed/ignored local deploy script.

--- deploy_plugin.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-curl https://api.graphdat.com/v1/plugins/private/aws_sqs/nitzanm/boundary-plugin-aws-sqs-python -X PUT -u "bug.boundary@bugbug.me:api.01f4f93c01-2843"
-
-rem pause
-rem curl https://premium-api.boundary.com/v1/plugins -X GET -u "bug.boundary@bugbug.me:api.01f4f93c01-2843"
